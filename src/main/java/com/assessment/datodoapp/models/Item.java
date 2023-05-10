b4c86787e1ca4711115d257c971dbfef7ba50b02
Jerome Wong <jeromewongjh@gmail.com>
2023-05-11 02:12:25 +0800

    feat: item model

---
+++ src/main/java/com/assessment/datodoapp/models/Item.java
@@ -0,0 +1,41 @@
+package com.assessment.datodoapp.models;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+public class Item implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter
+    @Setter
+    private Long id;
+
+    @Getter
+    @Setter
+    private String title;
+
+    @Getter
+    @Setter
+    private String description;
+
+    @Getter
+    @Setter
+    private boolean completed;
+
+    //creating default constructor
+    public Item() {
+    }
+    //for printing to console
+    @Override
+    public String toString() {
+        return "Item [id=" + id + ", title=" + title + ", description=" + description + ", completed=" + completed
+                + "]";
+    }
+}
