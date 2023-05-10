889da04b2fe9a7e32a808b9da0ae532f52acfee5
Jerome Wong <jeromewongjh@gmail.com>
2023-05-11 02:10:17 +0800

    chore: setup

---
+++ src/main/java/com/assessment/datodoapp/DaTodoAppApplication.java
@@ -0,0 +1,13 @@
+package com.assessment.datodoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaTodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaTodoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assessment/datodoapp/DaTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment.datodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaTodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
